f3ed5ad1613bb17584c11e078f68752c5dc2bd17
Will Weatherford <weatherford.william@gmail.com>
2016-09-19 00:37:01 -0700

    Updated Photo columns.

--- photos/models.py
+++ photos/models.py
@@ -5,6 +5,8 @@ from django.db import models as md
 class Photo(md.Model):
     """Data for one photo from a NASA Mars Rover."""
 
-    nasa_id = md.IntegerField()
+    # Make this the primary key?
+    nasa_id = md.IntegerField(unique=True)
     sol = md.IntegerField()
     earth_date = md.DateField()
+    img_src = md.URLField(max_length=400)
