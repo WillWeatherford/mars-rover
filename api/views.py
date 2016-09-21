c0f38c4fb81e478c505a4cfbf5230c86cf2fb039
Will Weatherford <weatherford.william@gmail.com>
2016-09-21 12:52:41 -0700

    Established rovers route and rover serializer

--- api/views.py
+++ api/views.py
@@ -2,11 +2,11 @@
 from __future__ import unicode_literals
 from rest_framework.generics import RetrieveAPIView
 from photos.models import Photo, Rover
-from .serializers import PhotoSerializer, RoverSerializer
+from .serializers import PhotoSerializer
 
 DEFAULTS = {
     'sol': 0,
-    'camera__name': 'FHAZ', 
+    'camera__name': 'FHAZ',
 }
 
 
@@ -18,15 +18,29 @@ class PhotoView(RetrieveAPIView):
 
 
 class RoverView(RetrieveAPIView):
-    """Return photo with associated next, prev and adjacent photos."""
+    """Use call to Rover to get photos."""
 
     queryset = Rover.objects.all()
-    serializer_class = RoverSerializer
+    serializer_class = PhotoSerializer
 
-    def retrieve(request, *args, **kwargs):
-        """Extend method to retrieve the rover."""
-        params = request.GET.dict()
-        # earth_date
-        # sol
-        # camera
+    def get_object(self, *args, **kwargs):
+        """Extend get_object method."""
+        rover = super(RoverView, self).get_object(*args, **kwargs)
+        req_params = self.request.GET.dict()
+        queryset = rover.photos
 
+        # camera_name = req_params.pop('camera__name')
+        # if camera_name:
+        #     queryset = rover.cameras.get(name=camera_name).photos
+        # else:
+        #     queryset = rover.photos
+
+        resp_params = DEFAULTS.copy()
+        resp_params.update(req_params)
+
+        if 'earth_date' in resp_params and 'sol' in resp_params:
+            resp_params.pop('sol')
+
+        # order_by?
+        queryset = queryset.filter(**resp_params)
+        return queryset.first()
