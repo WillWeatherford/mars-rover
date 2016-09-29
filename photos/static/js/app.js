d72b516a5399be87cfeb3789c0befe6800ff654a
muniri92 <mibrah04@gmail.com>
2016-09-29 16:30:31 -0700

    made changes that work, except curiosity

--- photos/static/js/app.js
+++ photos/static/js/app.js
@@ -8,7 +8,7 @@ var camButtons = [];
 var curiosity = 'Curiosity';
 var opportunity = 'Opportunity';
 var spirit = 'Spirit';
-
+var mainRover;
 var sol = 0;
 
 
@@ -19,18 +19,21 @@ var roverURL = '/api/rovers/';
 // GET THE ROVER FROM DAY 1
 $('#Opportunity').on('click', function(e){
     e.preventDefault();
+    mainRover = 'Opportunity';
     getRover(roverURL, 'Opportunity');
 })
 
 // GET THE ROVER FROM DAY 1
 $('#Curiosity').on('click', function(e){
     e.preventDefault();
+    mainRover = 'Curiosity';
     getRover(roverURL, 'Curiosity');
 })
 
 // GET THE ROVER FROM DAY 1
 $('#Spirit').on('click', function(e){
     e.preventDefault();
+    mainRover = 'Spirit';
     getRover(roverURL, 'Spirit');
 })
 
@@ -66,19 +69,10 @@ function getRover(url, rover) {
             // });
 
             for (var i = 0; i < response.concurrent.length; i++) {
-
                 $('#concur_container').append('<div class="div_concur row" id="div_' + i + '"></div>')
-
                 $('#div_' + i ).append('<h3 class="diff_camera">' + response.concurrent[i].camera.full_name + '</h3>')
-                // if (response.concurrent[i].img_src === 'notreal.jpg') {
-                //     $('#div_' + i ).append('<img src="/static/img/null_rover.jpg" class="concurrent_img">');
-                //     $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-
-                // } else {
-                    console.log(response.concurrent[i].img_src)
-                    $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-                // }
+                $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
+                $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
                 camButtons.push(i);
             }
             currentPhoto = response;
@@ -105,12 +99,8 @@ function fetchPhotos(url) {
         type: 'GET',
         dataType: 'json',
         success: function(response){
-            // if (response.next_photo === null || response.prev_photo === null) {
-            //     $('#main-photo').attr('src', '/static/img/null_rover.jpg');
-            // } else {
-            console.log(response)
+
             $('#main-photo').attr('src', response.img_src);
-            // }
             $('#camera_name').text('Camera: ' + response.camera.full_name);
             $('#sol_date').text('Sol Date: ' + response.sol);
             $('#earth_date').text('Earth Date: ' + response.earth_date);
@@ -127,18 +117,10 @@ function fetchPhotos(url) {
             // });
 
             for (var i = 0; i < response.concurrent.length; i++) {
-
                 $('#concur_container').append('<div class="div_concur" id="div_' + i + '"></div>');
-                
                 $('#div_' + i ).append('<h3 class="diff_camera">' + response.concurrent[i].camera.full_name + '</h3>')
-                if (response.concurrent[i].img_src === 'notreal.jpg') {
-                    $('#div_' + i ).append('<img src="/static/img/null_rover.jpg" class="concurrent_img">');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-
-                } else {
-                    $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-                }
+                $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
+                $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
                 camButtons.push(i);
             }
 
@@ -155,7 +137,6 @@ function fetchPhotos(url) {
     });
 }
 
-// fetchPhotos(photoURL)
 
 // GET THE NEXT PHOTO
 function nextPhoto() {
@@ -180,8 +161,6 @@ $('#prev-photo').on('click', function(){
 });
 
 
-
-
 // FETCH THE ROVER
 function fetchRover(url, rover) {
     $.ajax({
@@ -196,8 +175,6 @@ function fetchRover(url, rover) {
 }
 
 
-
-
 function fetchRoverBySol(url, rover, sol, cam) {
     console.log(url, rover, sol, cam)
     $.ajax({
@@ -207,12 +184,8 @@ function fetchRoverBySol(url, rover, sol, cam) {
         type: 'GET',
         dataType: 'json',
         success: function(response){
-            console.log(response)
-            if (response.next_photo === null) {
-                $('#main-photo').attr('src', '/static/img/null_rover.jpg');
-            } else {
-                $('#main-photo').attr('src', response.img_src);
-            }
+
+            $('#main-photo').attr('src', response.img_src);
             $('#sol_date').text('Sol Date: ' + response.sol);
             $('#earth_date').text('Earth Date: ' + response.earth_date);
         	currentPhoto = response;
@@ -229,18 +202,10 @@ function fetchRoverBySol(url, rover, sol, cam) {
             // });
 
             for (var i = 0; i < response.concurrent.length; i++) {
-
                 $('#concur_container').append('<div class="div_concur" id="div_' + i + '"></div>');
-                
                 $('#div_' + i ).append('<h3 class="diff_camera">' + response.concurrent[i].camera.full_name + '</h3>')
-                if (response.concurrent[i].img_src === 'notreal.jpg') {
-                    $('#div_' + i ).append('<img src="/static/img/null_rover.jpg" class="concurrent_img">');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-
-                } else {
-                    $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-                }
+                $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
+                $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
                 camButtons.push(i);
             }
 
@@ -258,71 +223,49 @@ function fetchRoverBySol(url, rover, sol, cam) {
     });
 }
 
-$('#next-sol').on('click', function(){
-    // fetchPhotos()
-	fetchRoverBySol(roverURL, opportunity, (currentPhoto.sol + 1), currentPhoto.camera.name);
-})
-
-$('#prev-sol').on('click', function(){
-	fetchRoverBySol(roverURL, opportunity, (currentPhoto.sol - 1), currentPhoto.camera.name);
-})
 
 $('#submit_sol').on('click', function(e){
 	e.preventDefault();
 	var soles = document.getElementById('sol');
 	new_sol = soles.elements[0].value;
-	fetchBySol(roverURL, opportunity, new_sol, currentPhoto.camera.name);
+    console.log(new_sol)
+	fetchRoverBySol(roverURL, mainRover, new_sol, currentPhoto.camera.name);
 });
-
-
 
 
 function fetchBySol(url, rover, sol, cam) {
     console.log(url, rover, sol, cam)
 	$.ajax({
-        url: url + rover
-        	 + '?sol=' + sol 
-        	 + '&camera__name=' + cam, 
+        url: url + rover + '?sol=' + sol + '&camera__name=' + cam, 
         type: 'GET',
         dataType: 'json',
         success: function(response){
-
+            console.log(url + rover + '?sol=' + sol + '&camera__name=' + cam)
+            console.log('response: ', response)
             $('#sol_input').val('');
-            console.log(response);
-        	if (response.img_src === 'notreal.jpg') {
-                $('#main-photo').attr('src', '/static/img/null_rover.jpg');
-            } else {
-                $('#main-photo').attr('src', response.img_src);
-            }
+
+            $('#main-photo').attr('src', response.img_src);
             $('#sol_date').text('Sol Date: ' + response.sol);
             $('#earth_date').text('Earth Date: ' + response.earth_date);
 
         	currentPhoto = response;
         	
-                        $('.diff_camera').remove();
+            $('.diff_camera').remove();
             $('.concurrent_img').remove();
             for (var i = 0; i < response.concurrent.length; i++) {
                 $('#cam_button_' + i).remove();
                 $('.' + i).remove();
             }
 
-            // $(document).ready(function () {
-            //     window.scrollTo(0,0);
-            // });
+            $(document).ready(function () {
+                window.scrollTo(0,0);
+            });
 
             for (var i = 0; i < response.concurrent.length; i++) {
-
                 $('#concur_container').append('<div class="div_concur" id="div_' + i + '"></div>');
-                
                 $('#div_' + i ).append('<h3 class="diff_camera">' + response.concurrent[i].camera.full_name + '</h3>')
-                if (response.concurrent[i].img_src === 'notreal.jpg') {
-                    $('#div_' + i ).append('<img src="/static/img/null_rover.jpg" class="concurrent_img">');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-
-                } else {
-                    $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
-                    $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
-                }
+                $('#div_' + i ).append('<img class="' + i + ' concurrent_img" src="' + response.concurrent[i].img_src + '" >');
+                $('#div_' + i ).append('<button style="width: 250px;" type="button" name="button" id="cam_button_' + i + '" class="button-primary">Switch Camera</button>');
                 camButtons.push(i);
             }
 
